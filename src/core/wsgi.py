3a47b0f65a08b164e4390d2fdf5c6d609c95b316
joab0 <joab0@proton.me>
2025-09-18 20:42:13 -0300

    feat: setup rest framework and improve code structure

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pupil_questions project.
+WSGI config for Pupil Questions project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pupil_questions.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
