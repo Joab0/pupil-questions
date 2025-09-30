16f5d1dd7785b4af9a6b7d81c378397798f26fe6
joab0 <joab0@proton.me>
2025-09-30 11:59:34 -0300

    feat: add turnsile to generate questions form

--- backend/apps/questions/forms.py
+++ backend/apps/questions/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from turnstile.fields import TurnstileField
 
 
 class QuestionSetAddForm(forms.Form):
+    turnstile = TurnstileField()
+
     title = forms.CharField(min_length=2, max_length=100, strip=True)
     prompt = forms.CharField(min_length=10, max_length=2048, strip=True)
     questions_number = forms.IntegerField(min_value=2, max_value=10, initial=5)
