b7af9b9002de4795db130f36fddbdb58c25ad466
joab0 <joab0@proton.me>
2025-09-30 00:39:06 -0300

    feat: add stats

--- backend/apps/dashboard/views.py
+++ backend/apps/dashboard/views.py
@@ -2,7 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from apps.questions.models import QuestionSet
+
 
 @login_required
 def dashboard_view(request: HttpRequest):
-    return render(request, "dashboard/dashboard.html")
+    questions_count = QuestionSet.objects.filter(user=request.user).count()
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        context={"questions_count": questions_count},
+    )
