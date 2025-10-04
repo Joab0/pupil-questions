81c15363878bdb3566490bacd2c757266bc5fb66
joab0 <joab0@proton.me>
2025-10-04 00:27:52 -0300

    feat: improve admin

--- backend/apps/questions/admin.py
+++ backend/apps/questions/admin.py
@@ -5,26 +5,24 @@ from .models import Choice, PracticeAnswer, PracticeSession, Question, QuestionS
 
 @admin.register(QuestionSet)
 class QuestionSetAdmin(admin.ModelAdmin):
-    list_display = ["user", "title", "model", "created_at", "status"]
     readonly_fields = ["created_at"]
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ["question_set", "text", "type", "created_at"]
     readonly_fields = ["created_at"]
 
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ["question", "text", "is_correct"]
+    pass
 
 
 @admin.register(PracticeSession)
 class PracticeSessionAdmin(admin.ModelAdmin):
-    list_display = ["user", "question_set", "current_index", "created_at"]
+    pass
 
 
 @admin.register(PracticeAnswer)
 class PracticeAnswerAdmin(admin.ModelAdmin):
-    list_display = ["session", "question", "choice"]
+    pass
