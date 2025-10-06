e7c1272d3601175c2608253844363b8a70e80c02
joab0 <joab0@proton.me>
2025-10-06 02:22:06 -0300

    feat: add practice stats

--- backend/apps/dashboard/views.py
+++ backend/apps/dashboard/views.py
@@ -1,15 +1,31 @@
+from datetime import timedelta
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render
 
-from apps.questions.models import QuestionSet
+from apps.questions.models import PracticeSession, Question
 
 
 @login_required
 def dashboard_view(request: HttpRequest):
-    questions_count = QuestionSet.objects.filter(user=request.user).count()
+    questions_count = Question.objects.filter(question_set__user=request.user).count()
+    practice_sessions = PracticeSession.objects.filter(question_set__user=request.user).all()
+    practices_count = practice_sessions.count()
+
+    practice_time = timedelta()
+    for session in practice_sessions:
+        if session.finished_at is None:
+            continue
+
+        practice_time += session.finished_at - session.created_at
+
     return render(
         request,
         "dashboard/dashboard.html",
-        context={"questions_count": questions_count},
+        context={
+            "questions_count": questions_count,
+            "practices_count": practices_count,
+            "practice_time": practice_time,
+        },
     )
